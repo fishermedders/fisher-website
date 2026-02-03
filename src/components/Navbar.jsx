3ff8fab1c9af554de200038e2b87d253184247cf
Fisher <fishermedders@gmail.com>
2026-02-03 04:01:04 -0500

    seggsy hero container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,32 +5,37 @@ export default function Navbar() {
   return (
     <header>
       <nav className="navbar" aria-label="Main navigation">
-        <ul>
-          <li>
+        <div className="nav-container">
+          {/* Brand Section: Stays on the left */}
+          <div className="nav-brand">
             <span>
               <b>fm</b>
             </span>
-          </li>
-          <li>|</li>
-          <li>
-            <Link to="/">home</Link>
-          </li>
-          <li>
-            <Link to="/fisher">fisher</Link>
-          </li>
-          <li>
-            <Link to="/music">music</Link>
-          </li>
-          <li>
-            <Link to="/movies">movies</Link>
-          </li>
-          <li>
-            <Link to="/television">television</Link>
-          </li>
-          <li>
-            <Link to="/site">site</Link>
-          </li>
-        </ul>
+            <span className="separator">|</span>
+          </div>
+
+          {/* Links Section: Wraps as needed */}
+          <ul className="nav-links">
+            <li>
+              <Link to="/">home</Link>
+            </li>
+            <li>
+              <Link to="/fisher">fisher</Link>
+            </li>
+            <li>
+              <Link to="/music">music</Link>
+            </li>
+            <li>
+              <Link to="/movies">movies</Link>
+            </li>
+            <li>
+              <Link to="/television">television</Link>
+            </li>
+            <li>
+              <Link to="/site">site</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
